9bc1d35a405bb6a49c038a78028abbb59ebcb6ea
tkrilavicius <tkrilavicius@ba6c9283-364f-4024-aa71-caa0555a0d25>
2010-04-06 21:38:16 +0000

    Snowball code of stemmer (Krilavičius, Petkevičius and Medelis version); C libraries; Java libraries. To compile the complete systems, please download Snowball dev from http://snowball.tartarus.org/

---
+++ libstemmer/c/lithuanian.h
@@ -0,0 +1,16 @@
+
+/* This file was generated automatically by the Snowball to ANSI C compiler */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern struct SN_env * create_env(void);
+extern void close_env(struct SN_env * z);
+
+extern int stem(struct SN_env * z);
+
+#ifdef __cplusplus
+}
+#endif
+
